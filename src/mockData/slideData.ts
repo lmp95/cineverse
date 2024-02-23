3c6bd4a0d67e25dfa10ebfd4c46891d86b3f6317
Lwin Maung Phyo <lwinmgphyo95@gmail.com>
2024-02-23 15:41:26 +0630

    ignore eslint mock folder

--- src/mockData/slideData.ts
+++ src/mockData/slideData.ts
@@ -1,87 +1,87 @@
 export const SLIDE_DATA = [
-  {
-    alt: "Soul",
-    path: "/images/soul.jpg",
-    desc: "Joe is a middle-school band teacher whose life hasn't quite gone the way he expected. His true passion is jazz -- and he's good. But when he travels to another realm to help someone find their passion, he soon discovers what it means to have soul.",
-  },
-  {
-    alt: "Ratatouille",
-    path: "/images/ratatouille.jpeg",
-    desc: "Remy, a rat, aspires to become a renowned French chef. However, he fails to realise that people despise rodents and will never enjoy a meal cooked by him.",
-  },
-];
+	{
+		alt: "Soul",
+		path: "/images/soul.jpg",
+		desc: "Joe is a middle-school band teacher whose life hasn't quite gone the way he expected. His true passion is jazz -- and he's good. But when he travels to another realm to help someone find their passion, he soon discovers what it means to have soul.",
+	},
+	{
+		alt: "Ratatouille",
+		path: "/images/ratatouille.jpeg",
+		desc: "Remy, a rat, aspires to become a renowned French chef. However, he fails to realise that people despise rodents and will never enjoy a meal cooked by him.",
+	},
+]
 
 export const UPCOMING_MOVIES = [
-  {
-    id: "hdg3n12x92w",
-    alt: "Orion and the Dark",
-    path: "/images/upcoming_1.jpg",
-    desc: "The thing Orion fears the most is the dark. When the embodiment of his worst fear pays a visit, Dark whisks Orion away on a roller-coaster ride around the world to prove there is nothing to be afraid of at night.",
-  },
-  {
-    id: "xf51hss923",
-    alt: "Badland Hunters",
-    path: "/images/upcoming_2.jpg",
-    desc: "After a major earthquake, Seoul, South Korea, transforms into an apocalyptic wasteland where everything from civilization to law and order has collapsed.",
-  },
-  {
-    id: "ys6381msnw1pa4",
-    alt: "Captain Miller",
-    path: "/images/upcoming_3.jpg",
-    desc: "A renegade Captain and his unconventional outlaws execute daring heists in the 1930s and 1940s. Miller must decide whether to continue running or confront the challenges head-on.",
-  },
-  {
-    id: "4341snuew83",
-    alt: "Upgraded",
-    path: "/images/upcoming_5.jpg",
-    desc: "An aspiring art intern is invited on a last-minute work trip to London, where she meets a handsome stranger.",
-  },
-  {
-    id: "56xf35jsm9",
-    alt: "Lift",
-    path: "/images/upcoming_6.jpg",
-    desc: "A master thief is wooed by his ex-girlfriend and the FBI to pull off an impossible heist with his international crew on a 777 passenger flight from London to Zurich.",
-  },
-  {
-    id: "601mzgh564jsu",
-    alt: "Madame Web",
-    path: "/images/upcoming_7.jpg",
-    desc: "Cassandra Webb is a New York City paramedic who starts to show signs of clairvoyance. Forced to confront revelations about her past, she must protect three young women from a mysterious adversary who wants them dead.",
-  },
-  {
-    id: "bxh671x7xka8",
-    alt: "Civil War",
-    path: "/images/upcoming_8.jpg",
-    desc: "From the writer of 28 Days Later, Ex Machina, Annihilation. Written and directed by Alex Garland.",
-  },
-  {
-    id: "n7zz620al3",
-    alt: "Deadpool & Wolverine",
-    path: "/images/upcoming_9.jpeg",
-    desc: "Wolverine is recovering from his injuries when he crosses paths with the loudmouth, Deadpool. They team up to defeat a common enemy.",
-  },
-  {
-    id: "kmal9201js661",
-    alt: "Gladiator 2",
-    path: "/images/upcoming_10.jpg",
-    desc: "Gladiator 2 is an upcoming epic historical drama film directed by Ridley Scott. Serving as a sequel to Gladiator (2000), the film was written by David Scarpa, and stars Paul Mescal, Denzel Washington, Pedro Pascal, Joseph Quinn, Fred Hechinger, May Calamawy, Connie Nielsen, and Derek Jacobi, the last two reprising their roles.",
-  },
-  {
-    id: "a61k912kxs0",
-    alt: "The Garfield Movie",
-    path: "/images/upcoming_11.jpg",
-    desc: "Garfield has an unexpected reunion with his long-lost father, a scruffy street cat who draws him into a high-stakes heist.",
-  },
-  {
-    id: "1091lsau7c1",
-    alt: "Despicable Me 4",
-    path: "/images/upcoming_12.jpg",
-    desc: "Gru welcomes a new member to the family, Gru Jr., who's intent on tormenting his dad. However, their peaceful existence soon comes crashing down when criminal mastermind Maxime Le Mal escapes from prison and vows revenge against Gru.",
-  },
-  {
-    id: "1zfrrw17412",
-    alt: "Imaginary Friends",
-    path: "/images/upcoming_13.jpg",
-    desc: "After discovering she can see everyone's imaginary friends, a girl embarks on a magical adventure to reconnect forgotten IFs with their kids.",
-  },
-];
+	{
+		id: "hdg3n12x92w",
+		alt: "Orion and the Dark",
+		path: "/images/upcoming_1.jpg",
+		desc: "The thing Orion fears the most is the dark. When the embodiment of his worst fear pays a visit, Dark whisks Orion away on a roller-coaster ride around the world to prove there is nothing to be afraid of at night.",
+	},
+	{
+		id: "xf51hss923",
+		alt: "Badland Hunters",
+		path: "/images/upcoming_2.jpg",
+		desc: "After a major earthquake, Seoul, South Korea, transforms into an apocalyptic wasteland where everything from civilization to law and order has collapsed.",
+	},
+	{
+		id: "ys6381msnw1pa4",
+		alt: "Captain Miller",
+		path: "/images/upcoming_3.jpg",
+		desc: "A renegade Captain and his unconventional outlaws execute daring heists in the 1930s and 1940s. Miller must decide whether to continue running or confront the challenges head-on.",
+	},
+	{
+		id: "4341snuew83",
+		alt: "Upgraded",
+		path: "/images/upcoming_5.jpg",
+		desc: "An aspiring art intern is invited on a last-minute work trip to London, where she meets a handsome stranger.",
+	},
+	{
+		id: "56xf35jsm9",
+		alt: "Lift",
+		path: "/images/upcoming_6.jpg",
+		desc: "A master thief is wooed by his ex-girlfriend and the FBI to pull off an impossible heist with his international crew on a 777 passenger flight from London to Zurich.",
+	},
+	{
+		id: "601mzgh564jsu",
+		alt: "Madame Web",
+		path: "/images/upcoming_7.jpg",
+		desc: "Cassandra Webb is a New York City paramedic who starts to show signs of clairvoyance. Forced to confront revelations about her past, she must protect three young women from a mysterious adversary who wants them dead.",
+	},
+	{
+		id: "bxh671x7xka8",
+		alt: "Civil War",
+		path: "/images/upcoming_8.jpg",
+		desc: "From the writer of 28 Days Later, Ex Machina, Annihilation. Written and directed by Alex Garland.",
+	},
+	{
+		id: "n7zz620al3",
+		alt: "Deadpool & Wolverine",
+		path: "/images/upcoming_9.jpeg",
+		desc: "Wolverine is recovering from his injuries when he crosses paths with the loudmouth, Deadpool. They team up to defeat a common enemy.",
+	},
+	{
+		id: "kmal9201js661",
+		alt: "Gladiator 2",
+		path: "/images/upcoming_10.jpg",
+		desc: "Gladiator 2 is an upcoming epic historical drama film directed by Ridley Scott. Serving as a sequel to Gladiator (2000), the film was written by David Scarpa, and stars Paul Mescal, Denzel Washington, Pedro Pascal, Joseph Quinn, Fred Hechinger, May Calamawy, Connie Nielsen, and Derek Jacobi, the last two reprising their roles.",
+	},
+	{
+		id: "a61k912kxs0",
+		alt: "The Garfield Movie",
+		path: "/images/upcoming_11.jpg",
+		desc: "Garfield has an unexpected reunion with his long-lost father, a scruffy street cat who draws him into a high-stakes heist.",
+	},
+	{
+		id: "1091lsau7c1",
+		alt: "Despicable Me 4",
+		path: "/images/upcoming_12.jpg",
+		desc: "Gru welcomes a new member to the family, Gru Jr., who's intent on tormenting his dad. However, their peaceful existence soon comes crashing down when criminal mastermind Maxime Le Mal escapes from prison and vows revenge against Gru.",
+	},
+	{
+		id: "1zfrrw17412",
+		alt: "Imaginary Friends",
+		path: "/images/upcoming_13.jpg",
+		desc: "After discovering she can see everyone's imaginary friends, a girl embarks on a magical adventure to reconnect forgotten IFs with their kids.",
+	},
+]
--- src/mockData/theaterSeats.ts
+++ src/mockData/theaterSeats.ts
@@ -1,487 +1,489 @@
+/* eslint-disable no-unused-vars */
+
 export enum SEAT_STATUS {
-  AVAILABLE = 1,
-  UNAVAILABLE = 2,
-  NO_SEAT = -1,
-  RESERVED = 3,
+	AVAILABLE = 1,
+	UNAVAILABLE = 2,
+	NO_SEAT = -1,
+	RESERVED = 3,
 }
 export enum SEAT_TYPE {
-  DOUBLE = "double",
-  SINGLE = "single",
+	DOUBLE = "double",
+	SINGLE = "single",
 }
 
 export const THEATER_SEATS = [
-  {
-    row: "a",
-    seats: [
-      {
-        no: 1,
-        status: SEAT_STATUS.NO_SEAT,
-      },
-      {
-        no: 2,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 3,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 4,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 5,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 6,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 7,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 8,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 9,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 10,
-        status: SEAT_STATUS.NO_SEAT,
-      },
-    ],
-  },
-  {
-    row: "b",
-    seats: [
-      {
-        no: 1,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 2,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 3,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 4,
-        status: SEAT_STATUS.RESERVED,
-      },
-      {
-        no: 5,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 6,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 7,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 8,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 9,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 10,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-    ],
-  },
-  {
-    row: "c",
-    seats: [
-      {
-        no: 1,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 2,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 3,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 4,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 5,
-        status: SEAT_STATUS.RESERVED,
-      },
-      {
-        no: 6,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 7,
-        status: SEAT_STATUS.RESERVED,
-      },
-      {
-        no: 8,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 9,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 10,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-    ],
-  },
-  {
-    row: "d",
-    seats: [
-      {
-        no: 1,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 2,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 3,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 4,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 5,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 6,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 7,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 8,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 9,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 10,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-    ],
-  },
-  {
-    row: "e",
-    seats: [
-      {
-        no: 1,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 2,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 3,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 4,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 5,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 6,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 7,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 8,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 9,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 10,
-        status: SEAT_STATUS.RESERVED,
-      },
-    ],
-  },
-  {
-    row: "f",
-    seats: [
-      {
-        no: 1,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 2,
-        status: SEAT_STATUS.RESERVED,
-      },
-      {
-        no: 3,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 4,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 5,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 6,
-        status: SEAT_STATUS.RESERVED,
-      },
-      {
-        no: 7,
-        status: SEAT_STATUS.RESERVED,
-      },
-      {
-        no: 8,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 9,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 10,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-    ],
-  },
-  {
-    row: "g",
-    seats: [
-      {
-        no: 1,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 2,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 3,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 4,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 5,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 6,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 7,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 8,
-        status: SEAT_STATUS.RESERVED,
-      },
-      {
-        no: 9,
-        status: SEAT_STATUS.RESERVED,
-      },
-      {
-        no: 10,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-    ],
-  },
-  {
-    row: "h",
-    seats: [
-      {
-        no: 1,
-        status: SEAT_STATUS.RESERVED,
-      },
-      {
-        no: 2,
-        status: SEAT_STATUS.NO_SEAT,
-      },
-      {
-        no: 3,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 4,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 5,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 6,
-        status: SEAT_STATUS.RESERVED,
-      },
-      {
-        no: 7,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 8,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 9,
-        status: SEAT_STATUS.NO_SEAT,
-      },
-      {
-        no: 10,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-    ],
-  },
-  {
-    row: "i",
-    seats: [
-      {
-        no: 1,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 2,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 3,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 4,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 5,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 6,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 7,
-        status: SEAT_STATUS.RESERVED,
-      },
-      {
-        no: 8,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 9,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 10,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-    ],
-  },
-  {
-    row: "s",
-    seats: [
-      {
-        no: 1,
-        status: SEAT_STATUS.NO_SEAT,
-      },
-      {
-        no: 2,
-        status: SEAT_STATUS.AVAILABLE,
-        type: SEAT_TYPE.DOUBLE,
-      },
-      {
-        no: 3,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 4,
-        status: SEAT_STATUS.NO_SEAT,
-      },
-      {
-        no: 5,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 6,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 7,
-        status: SEAT_STATUS.NO_SEAT,
-      },
-      {
-        no: 8,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 9,
-        status: SEAT_STATUS.AVAILABLE,
-      },
-      {
-        no: 10,
-        status: SEAT_STATUS.NO_SEAT,
-      },
-    ],
-  },
-];
+	{
+		row: "a",
+		seats: [
+			{
+				no: 1,
+				status: SEAT_STATUS.NO_SEAT,
+			},
+			{
+				no: 2,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 3,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 4,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 5,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 6,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 7,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 8,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 9,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 10,
+				status: SEAT_STATUS.NO_SEAT,
+			},
+		],
+	},
+	{
+		row: "b",
+		seats: [
+			{
+				no: 1,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 2,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 3,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 4,
+				status: SEAT_STATUS.RESERVED,
+			},
+			{
+				no: 5,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 6,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 7,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 8,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 9,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 10,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+		],
+	},
+	{
+		row: "c",
+		seats: [
+			{
+				no: 1,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 2,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 3,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 4,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 5,
+				status: SEAT_STATUS.RESERVED,
+			},
+			{
+				no: 6,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 7,
+				status: SEAT_STATUS.RESERVED,
+			},
+			{
+				no: 8,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 9,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 10,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+		],
+	},
+	{
+		row: "d",
+		seats: [
+			{
+				no: 1,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 2,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 3,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 4,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 5,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 6,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 7,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 8,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 9,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 10,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+		],
+	},
+	{
+		row: "e",
+		seats: [
+			{
+				no: 1,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 2,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 3,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 4,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 5,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 6,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 7,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 8,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 9,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 10,
+				status: SEAT_STATUS.RESERVED,
+			},
+		],
+	},
+	{
+		row: "f",
+		seats: [
+			{
+				no: 1,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 2,
+				status: SEAT_STATUS.RESERVED,
+			},
+			{
+				no: 3,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 4,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 5,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 6,
+				status: SEAT_STATUS.RESERVED,
+			},
+			{
+				no: 7,
+				status: SEAT_STATUS.RESERVED,
+			},
+			{
+				no: 8,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 9,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 10,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+		],
+	},
+	{
+		row: "g",
+		seats: [
+			{
+				no: 1,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 2,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 3,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 4,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 5,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 6,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 7,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 8,
+				status: SEAT_STATUS.RESERVED,
+			},
+			{
+				no: 9,
+				status: SEAT_STATUS.RESERVED,
+			},
+			{
+				no: 10,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+		],
+	},
+	{
+		row: "h",
+		seats: [
+			{
+				no: 1,
+				status: SEAT_STATUS.RESERVED,
+			},
+			{
+				no: 2,
+				status: SEAT_STATUS.NO_SEAT,
+			},
+			{
+				no: 3,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 4,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 5,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 6,
+				status: SEAT_STATUS.RESERVED,
+			},
+			{
+				no: 7,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 8,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 9,
+				status: SEAT_STATUS.NO_SEAT,
+			},
+			{
+				no: 10,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+		],
+	},
+	{
+		row: "i",
+		seats: [
+			{
+				no: 1,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 2,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 3,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 4,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 5,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 6,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 7,
+				status: SEAT_STATUS.RESERVED,
+			},
+			{
+				no: 8,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 9,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 10,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+		],
+	},
+	{
+		row: "s",
+		seats: [
+			{
+				no: 1,
+				status: SEAT_STATUS.NO_SEAT,
+			},
+			{
+				no: 2,
+				status: SEAT_STATUS.AVAILABLE,
+				type: SEAT_TYPE.DOUBLE,
+			},
+			{
+				no: 3,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 4,
+				status: SEAT_STATUS.NO_SEAT,
+			},
+			{
+				no: 5,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 6,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 7,
+				status: SEAT_STATUS.NO_SEAT,
+			},
+			{
+				no: 8,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 9,
+				status: SEAT_STATUS.AVAILABLE,
+			},
+			{
+				no: 10,
+				status: SEAT_STATUS.NO_SEAT,
+			},
+		],
+	},
+]
 
 export type SEAT = {
-  no: number;
-  row?: string;
-  status: SEAT_STATUS;
-};
+	no: number
+	row?: string
+	status: SEAT_STATUS
+}
 
 export const MONTHS = {
-  0: "JAN",
-  1: "FEB",
-  2: "MAR",
-  3: "APR",
-  4: "MAY",
-  5: "JUN",
-  6: "JUL",
-  7: "AUG",
-  8: "SEP",
-  9: "OCT",
-  10: "NOV",
-  11: "DEC",
-};
+	0: "JAN",
+	1: "FEB",
+	2: "MAR",
+	3: "APR",
+	4: "MAY",
+	5: "JUN",
+	6: "JUL",
+	7: "AUG",
+	8: "SEP",
+	9: "OCT",
+	10: "NOV",
+	11: "DEC",
+}
 
-export const SHOW_TIMES = ["9:00", "12:30", "14:45", "18:00", "21:30"];
+export const SHOW_TIMES = ["9:00", "12:30", "14:45", "18:00", "21:30"]
